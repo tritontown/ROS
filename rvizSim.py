a71d3c94ce4ac7a9ee3c4c0c29207412bce1c4ce
ruhtel <jarrett.ratelle@gmail.com>
2019-07-15 10:51:23 -0700

    Add files via upload

--- rvizSim.py
+++ rvizSim.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python
+
 import rospy
 #from cmvision.msg import Blobs, Blob
 from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
 from geometry_msgs.msg import Point
 from tf.transformations import quaternion_from_euler
-from geometry_msgs.msg import Pose
-#from sensor_msgs.msg import CameraInfo
+from geometry_msgs.msg import PoseStamped
+from tag_sim.msg import tagInfo
+from tag_sim.msg import tagInfoArray
 
 
 class MarkerBasics(object):
+    #__slots__ = ['tagData']
 
-    def __init__(self):
-        self.marker_objectlisher = rospy.Publisher('/marker_redball', Marker, queue_size=10)
+    marker_objectlisher = rospy.Publisher('/tagLoc', Marker, queue_size=10)
+
+    def __init__(self, tagData):
+        #self.marker_objectlisher = rospy.Publisher('/tagLoc', Marker, queue_size=10)
         self.rate = rospy.Rate(1)
-        self.init_marker(index=0,z_val=0)
+        #self.tagData = tagData
+        self.init_marker(tagData)
 
-    def init_marker(self,index=0, z_val=0):
+    def init_marker(self, tagData):
         self.marker_object = Marker()
         self.marker_object.header.frame_id = "/base_link"
         self.marker_object.header.stamp    = rospy.get_rostime()
-        self.marker_object.ns = "mira"
-        self.marker_object.id = index
+        self.marker_object.ns = "tag"
+        self.marker_object.id = tagData.id
         self.marker_object.type = Marker.CUBE
         self.marker_object.action = Marker.ADD
 
-        my_point = Point()
-        my_point.z = z_val
-        self.marker_object.pose.position = my_point
+        self.marker_object.pose.position.x = tagData.pose.pose.position.x
+        self.marker_object.pose.position.y = tagData.pose.pose.position.y
+        self.marker_object.pose.position.z = tagData.pose.pose.position.z
 
-        self.marker_object.pose.orientation.x = 1
-        self.marker_object.pose.orientation.y = 1
-        self.marker_object.pose.orientation.z = 0.0
-        self.marker_object.pose.orientation.w = 1.0
-        self.marker_object.scale.x = 10
-        self.marker_object.scale.y = 10
-        self.marker_object.scale.z = 0.000001
+        self.marker_object.pose.orientation.x = tagData.pose.pose.orientation.x
+        self.marker_object.pose.orientation.y = tagData.pose.pose.orientation.y
+        self.marker_object.pose.orientation.z = tagData.pose.pose.orientation.z
+        self.marker_object.pose.orientation.w = tagData.pose.pose.orientation.w
+        self.marker_object.scale.x = 5
+        self.marker_object.scale.y = 5
+        self.marker_object.scale.z = 5
 
         self.marker_object.color.r = 1.0
         self.marker_object.color.g = 0.0
@@ -46,82 +53,53 @@ class MarkerBasics(object):
         # If we want it for ever, 0, otherwise seconds before desapearing
         self.marker_object.lifetime = rospy.Duration(0)
 
-    def update_position(self,position,angle):       
-        self.marker_object.pose.position.x = position.x - 320
-        self.marker_object.pose.position.y = position.y - 240
+        self.marker_objectlisher.pubish(self.marker_object)
 
-        q = quaternion_from_euler(0,0,angle)
-        self.marker_object.pose.orientation.x = q[0]
-        self.marker_object.pose.orientation.y = q[1]
-        self.marker_object.pose.orientation.z = q[2]
-        self.marker_object.pose.orientation.w = q[3]
+    def update_position(self, tagData):       
+        self.marker_object.pose.position.x = tagData.pose.position.x
+        self.marker_object.pose.position.y = tagData.pose.position.y
+        self.marker_object.pose.position.z = tagData.pose.position.z
+
+        self.marker_object.pose.orientation.x = tagData.pose.orientation.x
+        self.marker_object.pose.orientation.y = tagData.pose.orientation.y
+        self.marker_object.pose.orientation.z = tagData.pose.orientation.z
+        self.marker_object.pose.orientation.w = tagData.pose.orientation.w
 
         self.marker_object.header.stamp = rospy.get_rostime()
         self.marker_objectlisher.publish(self.marker_object)
-        
 
-
-class BallDetector(object):
+class TagDetector(object):
+    
     def __init__(self):        
         self.rate = rospy.Rate(1)
-        #self.save_camera_values()        
-        rospy.Subscriber('tagPose', Pose, self.redball_detect_callback)        
-        self.markerbasics_object = MarkerBasics()
+        self.markArr = MarkerArray()
 
-    """
-    def save_camera_values(self):
-        data_camera_info = None
-        while data_camera_info is None:
-            data_camera_info = rospy.wait_for_message('/mira/mira/camera1/camera_info', CameraInfo, timeout=5)
-            rospy.loginfo("No Camera info found, trying again")
+        rospy.Subscriber('tagPose', tagInfoArray, self.tag_callback)        
+        
+   
+    def tag_callback(self,data):
     
-    
-        self.cam_height_y = data_camera_info.height
-        self.cam_width_x = data_camera_info.width
-        rospy.loginfo("CAMERA INFO:: Image width=="+str(self.cam_width_x)+", Image Height=="+str(self.cam_height_y))
-    """
-    
-    def redball_detect_callback(self,data):
+        for tag in data.tags:
+            
+            found = False
+            
+            for marker in self.markArr.markers:
 
-        """
-        if(len(data.blobs)):
+                print(tag.id)
+                print(marker.id)
+                if(tag.id == marker.id):
+                    marker.id.update_position(tag)
+                    found = True
 
-            for obj in data.blobs:
-                if obj.name == "RedBall":
-                    rospy.loginfo("Blob <"+str(obj.name)+"> Detected!")
-                    redball_point = Point()
-                    # There is a diference in the axis from blobs and the camera link frame.
-                    # We convert to percent of the screen
-                    # TODO: Take into account the Depth distance and camera cone.
-                    rospy.loginfo("self.cam_width_x="+str(self.cam_width_x))
-                    rospy.loginfo("self.cam_width_x="+str(self.cam_height_y))
-                    rospy.loginfo("obj.x="+str(obj.x))
-                    rospy.loginfo("obj.y="+str(obj.y))
+            if(~found):
+                self.markArr.markers.append(MarkerBasics(tag))
 
-                    middle_width = float(self.cam_width_x)/2.0 
-                    middle_height = float(self.cam_height_y)/2.0
-
-                    redball_point.x = (obj.x - middle_width) / float(self.cam_width_x)
-                    redball_point.z = (obj.y - middle_height) / float(self.cam_height_y)                    
-                    redball_point.y = 0.6            
-                    rospy.loginfo("blob is at Point="+str(redball_point))
-                    self.markerbasics_object.update_position(position=redball_point)
-
-
-        else: 
-             rospy.logwarn("No Blobs Found")
-    """
-
-        #redball_point = Point()
-        #redball_point.x = data.point.x
-        #redball_point.y = data.point.y
-        self.markerbasics_object.update_position(data.position, angle=data.orientation.z)
 
     def start_loop(self):
         # spin() simply keeps python from exiting until this node is stopped
         rospy.spin()
 
 if __name__ == '__main__':
-    rospy.init_node('redball_detections_listener_node', anonymous=True)
-    redball_detector_object = BallDetector()
-    redball_detector_object.start_loop()
+    rospy.init_node('tag_detections_listener_node', anonymous=True)
+    tag_detector_object = TagDetector()
+    tag_detector_object.start_loop()
---
+++ tagDet.py
@@ -0,0 +1,189 @@
+import apriltag
+import cv2
+import numpy as np
+import time
+import math
+import rospy
+from geometry_msgs.msg import PoseStamped
+from tag_sim.msg import tagInfoArray
+from tag_sim.msg import tagInfo
+from tf.transformations import quaternion_from_euler as makeQuat
+
+'''
+#draw dots on tag corners
+def draw_tag(frame,result):
+        angle = []
+        for i in range(len(result)):
+                cv2.circle(frame,tuple(result[i].center.astype(int)),5,(0,0,255),3)
+                cv2.circle(frame,tuple(result[i].corners[0].astype(int)),5,(0,0,255),3)
+                cv2.circle(frame,tuple(result[i].corners[1].astype(int)),5,(0,255,0),3)
+                cv2.circle(frame,tuple(result[i].corners[2].astype(int)),5,(255,0,0),3)
+                cv2.circle(frame,tuple(result[i].corners[3].astype(int)),5,(0,0,0),3)
+                mid = [np.int((result[i].corners[0][0]+result[i].corners[1][0])/2),np.int((result[i].corners[0][1]+result[i].corners[1][1])/2)]
+                cv2.line(frame,tuple(result[i].center.astype(int)),tuple(mid),(0,0,255),5)
+                angle.append(math.degrees(math.atan2(result[i].corners[1][1]-result[i].corners[0][1],result[i].corners[1][0]-result[i].corners[0][0])))
+        return frame, angle 
+'''
+
+#draw box on tags
+def draw_pose(overlay, camera_params, tag_size, pose, z_sign=1):
+    opoints = np.array([
+        -1, -1, 0,
+        1, -1, 0,
+        1,  1, 0,
+        -1,  1, 0,
+        -1, -1, -2*z_sign,
+        1, -1, -2*z_sign,
+        1,  1, -2*z_sign,
+        -1,  1, -2*z_sign,
+        ]).reshape(-1, 1, 3) * 0.5*tag_size
+
+    edges = np.array([
+        0, 1,
+        1, 2,
+        2, 3,
+        3, 0,
+        0, 4,
+        1, 5,
+        2, 6,
+        3, 7,
+        4, 5,
+        5, 6,
+        6, 7,
+        7, 4
+        ]).reshape(-1, 2)
+
+    fx, fy, cx, cy = camera_params
+
+    K = np.array([fx, 0, cx, 0, fy, cy, 0, 0, 1]).reshape(3, 3)
+
+    rvec, _ = cv2.Rodrigues(pose[:3,:3])
+    tvec = pose[:3, 3]
+
+    dcoeffs = np.zeros(5)
+
+    ipoints, _ = cv2.projectPoints(opoints, rvec, tvec, K, dcoeffs)
+
+    ipoints = np.round(ipoints).astype(int)
+
+    ipoints = [tuple(pt) for pt in ipoints.reshape(-1, 2)]
+
+    for i, j in edges:
+        cv2.line(overlay, ipoints[i], ipoints[j], (0, 255, 0), 1, 16)
+
+def isRotationMatrix(R):
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3,dtype=R.dtype)
+    n = np.linalg.norm(I-shouldBeIdentity)
+    return n<1e-6
+
+def tagDataToPose(pose, tagId):
+
+    angles = rotationMatrixToEulerAngles(pose[0:3,0:3])
+    center = pose[0:3, 3]
+
+    # Convert angles to Quaternion space
+    quat = makeQuat(angles[0], angles[1], angles[2])
+
+    # Init Pose Obj
+    tagPose = PoseStamped()
+
+    # Pose creation time saved, may be useful later
+    #tagPose.header = rospy.get_rostime()
+
+    # Position
+    tagPose.pose.position.x = center[0]
+    tagPose.pose.position.y = center[1]
+    tagPose.pose.position.z = center[2]
+
+    # Orientation
+    tagPose.pose.orientation.x = quat[0]
+    tagPose.pose.orientation.y = quat[1]
+    tagPose.pose.orientation.z = quat[2]
+    tagPose.pose.orientation.w = quat[3]
+
+    # This is fairly inefficient, but makes code easier to read
+    tag = tagInfo()
+    tag.pose = tagPose
+    tag.id = tagId
+
+    return tag
+
+def rotationMatrixToEulerAngles(R):
+    assert(isRotationMatrix(R))
+    sy = math.sqrt(R[0,0]*R[0,0]+R[1,0]*R[1,0])
+    singular = sy<1e-6
+    if not singular:
+        x = math.atan2(R[2,1],R[2,2])
+        y = math.atan2(-R[2,0],sy)
+        z = math.atan2(R[1,0],R[0,0])
+    else:
+        x = math.atan2(-R[1,2],R[1,1]) 
+        y = math.atan2(-R[2,0],sy)
+        z = 0
+    return np.array([x,y,z])
+
+def main():
+   
+    DIM = (640, 480)
+    camera_params = (506.66588415210174,507.57637424966526,311.03765199523536,238.60300515336095)
+    tag_size = 160
+    video_capture = cv2.VideoCapture(0)
+    detector = apriltag.Detector(searchpath=apriltag._get_demo_searchpath())
+   
+    # ROS Publisher Init
+    pub = rospy.Publisher('tagPose', tagInfoArray, queue_size=5)
+    rospy.init_node('talker', anonymous=True)
+
+    # Init array of tag poses
+    #tagPoses = tagInfoArray()
+
+    while (True):
+        
+        _, frame = video_capture.read()
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        result = detector.detect(gray_frame)
+
+        # Init fresh tag arr to pass
+        tagPoses = tagInfoArray()
+
+        '''
+        if (result):
+            frame, angle = draw_tag(frame,result)
+            print(angle)
+        '''
+
+
+        for i, detection in enumerate(result):
+            pose, e0, e1 = detector.detection_pose(detection,camera_params,tag_size)
+            draw_pose(frame, camera_params,tag_size,pose)
+
+            print(result[i].tag_id)
+            tagPoses.tags.append( tagDataToPose(pose, result[i].tag_id) )
+
+           # print(angles,pose[0:3,3])	
+
+        print(tagPoses)
+
+        pub.publish(tagPoses)
+
+        cv2.imshow("apriltags",frame)
+        k = cv2.waitKey(1)
+  
+        if( k % 256 == 32):
+            break
+
+        """
+        except KeyboardInterrupt:
+            break
+        """
+    video_capture.release()
+
+if __name__=="__main__":
+    
+    try:
+        main()
+
+    except rospy.ROSInterruptException:
+        pass
